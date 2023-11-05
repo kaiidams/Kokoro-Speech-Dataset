d776626d0d18cdb81a44dfc6e69318c417a6fcd3
Katsuya Iida <katsuya.iida@gmail.com>
2023-11-05 07:38:04 +0000

    Use stderr to print messages

--- extract.py
+++ extract.py
@@ -3,13 +3,17 @@ import sys
 import json
 import torchaudio
 import torch
-import re
 import argparse
+
+
+def err_print(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
+
 
 def read_params_list(data_dir, size):
     index_file = os.path.join(data_dir, 'index.json')
     if not os.path.exists(index_file):
-        print(f"{sys.argv[0]}: error: `data/index.json' is not found. Please download it from the project page.")
+        err_print("`data/index.json' is not found. Please download it from the project page.")
         sys.exit(1)
 
     with open(os.path.join(data_dir, 'index.json')) as f:
@@ -21,21 +25,23 @@ def read_params_list(data_dir, size):
         if (size == 'xlarge') or (size in params['sizes'].split())
     ]
 
+
 def check_data_directory(data_dir, params_list):
     isok = True
     for params in params_list:
         id_ = params['id']
         audio_dir = os.path.join(data_dir, f'{id_}')
         if not os.path.isdir(audio_dir):
-            print(f"{sys.argv[0]}: error: `{audio_dir}' is missing.")
+            err_print(f"`{audio_dir}' is missing.")
             isok = False
     if isok:
-        print("All audio directories exist.")
+        err_print("All audio directories exist.")
     return isok
 
+
 def dump_script(data_dir, params_list):
-    print('Run these commands to download archives.')
-    print()
+    err_print('Run these commands to download archives.')
+    err_print()
 
     print(f'cd {data_dir}')
 
@@ -48,6 +54,7 @@ def dump_script(data_dir, params_list):
         archive_url = params['archive_url']
         archive_file = os.path.basename(archive_url)
         print(f"unzip {archive_file} -d {id_}")
+
 
 def extract_wav_files(data_dir, params_list, clip_format, sample_rate, output_dir):
 
@@ -70,7 +77,7 @@ def extract_wav_files(data_dir, params_list, clip_format, sample_rate, output_di
                 audio_start, audio_end = int(audio_start), int(audio_end)
                 if current_file != audio_file:
                     file = os.path.join(audio_dir, audio_file)
-                    print(f'\rReading {file}', end='')
+                    err_print(f'\rReading {file}', end='')
                     y, sr = torchaudio.load(file)
                     assert len(y.shape) == 2 and y.shape[0] == 1
                     assert y.dtype == torch.float32
@@ -81,6 +88,7 @@ def extract_wav_files(data_dir, params_list, clip_format, sample_rate, output_di
                 output_file = os.path.join(output_dir, clip_dir, f'{id_}.{clip_ext}')
                 y = current_audio[:, audio_start:audio_end]
                 torchaudio.save(output_file, y, sample_rate)
+
 
 def write_metafile(data_dir, params_list, output_dir):
 
@@ -95,6 +103,7 @@ def write_metafile(data_dir, params_list, output_dir):
                     id_, _, _, _, text, voca = parts
                     metadata_f.write(f'{id_}|{text}|{voca}\n')
 
+
 def main(args):
 
     params_list = read_params_list(args.data_dir, args.size)
@@ -106,13 +115,16 @@ def main(args):
         extract_wav_files(args.data_dir, params_list, args.format, args.sample_rate, args.output_dir)
         write_metafile(args.data_dir, params_list, args.output_dir)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data-dir', default='data', help='Data directory')
     parser.add_argument('--output-dir', default='output', help='Output directory')
-    parser.add_argument('--size', default='tiny', choices=['tiny', 'small', 'large', 'xlarge'],
+    parser.add_argument(
+        '--size', default='tiny', choices=['tiny', 'small', 'large', 'xlarge'],
         help='Size name to extract')
-    parser.add_argument('--format', default='wav', choices=['wav', 'flac', 'mp3', 'ogg'],
+    parser.add_argument(
+        '--format', default='wav', choices=['wav', 'flac', 'mp3', 'ogg'],
         help='Format of clips')
     parser.add_argument('--sample-rate', type=int, default=22050, help='Expected sampling rate')
 
